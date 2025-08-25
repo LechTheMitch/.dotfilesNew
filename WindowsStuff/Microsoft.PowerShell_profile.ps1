411a2519cebb71ba6cbb5887cf14276038b2981d
LechTheMitch <104312143+LechTheMitch@users.noreply.github.com>
2025-08-25 12:05:31 +0300

    Initial Powershell Profile

--- WindowsStuff/Microsoft.PowerShell_profile.ps1
+++ WindowsStuff/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,5 @@
+$PSStyle.FileInfo.Directory = "`e[33m"
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineOption -BellStyle None
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+oh-my-posh init pwsh | Invoke-Expression
